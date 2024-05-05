2fae8eeb6cfea02e951bf4ab27c2c6471ed09402
paraIncog <selgeandreas02@gmail.com>
2024-05-05 17:27:40 +0300

    Redo country style in paragraph

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
+let countryTLDs = [];
+
 // Constants
 const result = document.getElementById("result");
 const locatorInput = document.getElementById("locatorInput");
-// const inputLCase = locatorInput.value.toLowerCase();
 
 // Event Listeners
-locatorInput.addEventListener("input", showResults);
 document.addEventListener("DOMContentLoaded", function () {
+  fetchLists().then(() => {
+    locatorInput.addEventListener("input", showResults);
+  });
   showResults();
-  fetchLists();
 });
+
+function fetchLists() {
+  return fetch(
+    "https://gist.githubusercontent.com/derlin/421d2bb55018a1538271227ff6b1299d/raw/3a131d47ca322a1d001f1f79333d924672194f36/country-codes-tlds.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      countryTLDs = data; // Store the fetched data globally
+    });
+}
 
 // Show Results
 function showResults() {
-  if (locatorInput.value.length < 3) {
+  const input = locatorInput.value.toLowerCase(); // Process input in lowercase
+  if (input.length < 3) {
     result.innerHTML = `<p class="res-bad">Too Short</p>`;
-  } else if (
-    locatorInput.value === "www" ||
-    locatorInput.value === "WWW" ||
-    locatorInput.value === "Www" ||
-    locatorInput.value === "wWw" ||
-    locatorInput.value === "wwW" ||
-    locatorInput.value === "WWw" ||
-    locatorInput.value === "WwW" ||
-    locatorInput.value === "wWW"
-  ) {
-    result.innerHTML = `<p class="res-good">This is an acronym for the World Wide Web</p>`;
-  } else if (locatorInput.value.endsWith(".com")) {
-	result.innerHTML = `<p class="res-good">Good URL</p><p>Short for: company</p>`;
+  } else if (input === 'www') {
+	result.innerHTML = `<p class="res-good">This is the acronym for the World Wide Web</p>`
   } else {
-	result.innerHTML = `No Input`;
+    let matchedCountry = countryTLDs.find((country) =>
+      country.tlds.some((tld) => input.endsWith(tld))
+    );
+    if (matchedCountry) {
+      result.innerHTML = `<p class="res-good">Country</p>${matchedCountry.country}`;
+    } else {
+      result.innerHTML = `<p class="res-bad">No matching country found for the TLD</p>`;
+    }
   }
-}
-
-function fetchLists() {
-  fetch(
-    "https://gist.githubusercontent.com/derlin/421d2bb55018a1538271227ff6b1299d/raw/3a131d47ca322a1d001f1f79333d924672194f36/country-codes-tlds.json"
-  ).then((response) => response.json());
 }
